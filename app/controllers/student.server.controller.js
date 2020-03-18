d441f0e694d6ce17391ec25bb81fd26555c5c061
Yeseul Jang <jangysseul@gmail.com>
2020-03-18 02:47:14 -0400

    add authentication

--- app/controllers/student.server.controller.js
+++ app/controllers/student.server.controller.js
@@ -1,10 +1,14 @@
 const Student = require('mongoose').model('Student');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/config');
+const jwtExpirySeconds = 300;
+const jwtKey =config.secretKey;
 
 exports.createStudent = function (req, res, next) {
   
     var student = new Student(req.body); //get data from ejs page and attaches them to the model
 
-    // Use the 'User' instance's 'save' method to save a new user document
     student.save(function (err) {
         if (err) {
             // Call the next middleware with an error message
@@ -49,4 +53,138 @@ exports.studentByID = function (req, res, next, id) {
 			next();
 		}
 	});
+};
+
+exports.authenticate = function(req, res, next) {
+	// Get credentials from request
+	console.log(req.body)
+	const studentNumber = req.body.auth.studentNumber;
+	const password  = req.body.auth.password;
+	console.log(password)
+	console.log(studentNumber)
+	//find the user with given studentNumber using static method findOne
+	Student.findOne({studentNumber: studentNumber}, (err, student) => {
+			if (err) {
+				return next(err);
+			} else {
+			console.log(student)
+			//compare passwords	
+			if(bcrypt.compareSync(password, student.password)) {
+				// Create a new token with the student id in the payload
+  				// and which expires 300 seconds after issue
+				const token = jwt.sign({ studentNumber: student.studentNumber }, jwtKey, 
+					{algorithm: 'HS256', expiresIn: jwtExpirySeconds });
+				console.log('token:', token)
+				// set the cookie as the token string, with a similar max age as the token
+				// here, the max age is in milliseconds
+				res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000,httpOnly: true});
+				res.status(200).send({ screen: student.studentNumber });
+
+				//call the next middleware
+				next()
+			} else {
+				res.json({status:"error", message: "Invalid studentNumber/password!!!",
+				data:null});
+			}
+			
+		}
+		
+	});
+};
+//check if the user is signed in
+exports.isSignedIn = (req, res) => {
+	// Obtain the session token from the requests cookies,
+	// which come with every request
+	const token = req.cookies.token
+	console.log(token)
+	// if the cookie is not set, return 'auth'
+	if (!token) {
+	  return res.send({ screen: 'auth' }).end();
+	}
+	var payload;
+	try {
+	  // Parse the JWT string and store the result in `payload`.
+	  // Note that we are passing the key in this method as well. This method will throw an error
+	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	  // or if the signature does not match
+	  payload = jwt.verify(token, jwtKey)
+	} catch (e) {
+	  if (e instanceof jwt.JsonWebTokenError) {
+		// the JWT is unauthorized, return a 401 error
+		return res.status(401).end()
+	  }
+	  // otherwise, return a bad request error
+	  return res.status(400).end()
+	}
+  
+	// Finally, token is ok, return the studentNumber given in the token
+	res.status(200).send({ screen: payload.studentNumber });
+}
+exports.signout = (req, res) => {
+	res.clearCookie("token")
+	return res.status('200').json({message: "signed out"})
+	// Redirect the user back to the main application page
+	//res.redirect('/');
+}
+
+exports.welcome = (req, res) => {
+	// We can obtain the session token from the requests cookies,
+	// which come with every request
+	const token = req.cookies.token
+  
+	// if the cookie is not set, return an unauthorized error
+	if (!token) {
+	  return res.status(401).end()
+	}
+   
+	var payload;
+	try {
+	  // Parse the JWT string and store the result in `payload`.
+	  // Note that we are passing the key in this method as well. This method will throw an error
+	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	  // or if the signature does not match
+	  payload = jwt.verify(token, jwtKey)
+	} catch (e) {
+	  if (e instanceof jwt.JsonWebTokenError) {
+		// if the error thrown is because the JWT is unauthorized, return a 401 error
+		return res.status(401).end()
+	  }
+	  // otherwise, return a bad request error
+	  return res.status(400).end()
+	}
+  
+
+	res.send(`${payload.studentNumber}!`)
+ };
+
+ //isAuthenticated() method to check whether a user is currently authenticated
+exports.requiresLogin = function (req, res, next) {
+    // Obtain the session token from the requests cookies,
+	// which come with every request
+	const token = req.cookies.token
+	console.log(token)
+	// if the cookie is not set, return an unauthorized error
+	if (!token) {
+	  return res.send({ screen: 'auth' }).end();
+	}
+	var payload;
+	try {
+	  // Parse the JWT string and store the result in `payload`.
+	  // Note that we are passing the key in this method as well. This method will throw an error
+	  // if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	  // or if the signature does not match
+	  payload = jwt.verify(token, jwtKey)
+	  console.log('in requiresLogin - payload:',payload)
+	  req.id = payload.id;
+	} catch (e) {
+	  if (e instanceof jwt.JsonWebTokenError) {
+		// if the error thrown is because the JWT is unauthorized, return a 401 error
+		return res.status(401).end()
+	  }
+	  // otherwise, return a bad request error
+	  return res.status(400).end()
+	}
+	// user is authenticated
+	//call next function in line
+    next();
 };
