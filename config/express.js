aaf7f1b69d83252610018268d2b29cbadefcb661
Yeseul Jang <jangysseul@gmail.com>
2020-03-18 16:16:48 -0400

    add controller

--- config/express.js
+++ config/express.js
@@ -59,6 +59,8 @@ module.exports = function () {
     // Load the routing files
     require('../app/routes/index.server.routes.js')(app);
     require('../app/routes/student.server.routes.js')(app);
+    require('../app/routes/course.server.routes.js')(app);
+    
     //The express.static() middleware takes one argument 
     //to determine the location of the static folder
     //Configure static file serving
